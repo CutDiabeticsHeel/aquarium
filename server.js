54acccdc7a10668fe00322194fde7c887be097a7
shishioZXC <niksizdog@gmail.com>
2026-07-05 21:24:59 +0300

    test69

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ import {getPlaybill, getPerformanceData, getTroupe,
     getActorData, getPerformances, getPerformancePageData, 
     getHrefPerformanceForActor, getStarringListFromPerformance, getReviews} from './database-function.js';
 
-//привет
 
 
 const config = JSON.parse(fs.readFileSync("./captcha.json", "utf-8"));
